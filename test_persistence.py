03d1cfea5287b1559003a6bebd703290aaeb4580
David M <munozram@users.noreply.github.com>
2023-12-16 03:27:57 +0000

    Automated commit after PEP8 formatting

--- test_persistence.py
+++ test_persistence.py
@@ -5,35 +5,37 @@ import pytest
 from PHN_Directed import *
 
 # Cyclic graph
-A1 = np.diag(np.ones((5,)),1)
-A1[-1,0] = 1
+A1 = np.diag(np.ones((5,)), 1)
+A1[-1, 0] = 1
 D1 = nx.DiGraph(A1)
-result1 = np.array([[    0.,  0.,  0.,  0.,  0.,  0.,  1.,  3.,  3.,  4.],
-                    [np.inf,  1.,  1.,  1.,  1.,  1.,  3.,  4.,  4.,  5.],
-                    [    0.,  0.,  0.,  0.,  0.,  0.,  1.,  2.,  2.,  4.]])
+result1 = np.array([[0., 0., 0., 0., 0., 0., 1., 3., 3., 4.],
+                    [np.inf, 1., 1., 1., 1., 1., 3., 4., 4., 5.],
+                    [0., 0., 0., 0., 0., 0., 1., 2., 2., 4.]])
 
 # Almost cyclic graph
-A2 = np.diag(np.ones((5,)),1)
-A2[-1,0] = 6
-A2[0,-1] = 1
+A2 = np.diag(np.ones((5,)), 1)
+A2[-1, 0] = 6
+A2[0, -1] = 1
 D2 = nx.DiGraph(A2)
-result2 = np.array([[    0.,  0.,  0.,  0.,  0.,  0.,  1.],
-                    [np.inf,  1.,  1.,  1.,  1.,  1.,  5.],
-                    [    0.,  0.,  0.,  0.,  0.,  0.,  1.]])
+result2 = np.array([[0., 0., 0., 0., 0., 0., 1.],
+                    [np.inf, 1., 1., 1., 1., 1., 5.],
+                    [0., 0., 0., 0., 0., 0., 1.]])
 
 # Random graph
-A3 = np.array([[0. , 1. , 0. , 1.6, 0. ],
-               [0. , 0. , 0. , 0. , 0. ],
-               [0. , 2. , 0. , 0. , 0. ],
-               [0. , 0. , 0.8, 0. , 0. ],
-               [0.5, 0. , 0. , 3. , 0. ]])
+A3 = np.array([[0., 1., 0., 1.6, 0.],
+               [0., 0., 0., 0., 0.],
+               [0., 2., 0., 0., 0.],
+               [0., 0., 0.8, 0., 0.],
+               [0.5, 0., 0., 3., 0.]])
 D3 = nx.DiGraph(A3)
-result3 = np.array([[   0.  , 0.  , 0.  , 0.  , 0.  , 2.  , 2.1 , 2.9 , 2.9 , 4.9 , 4.9 , 4.9 ],
-                    [ np.inf, 1.  , 1.6 , 0.8 , 0.5 , 4.4 , 3.  , 3.8 , 3.8 , 5.8 , 5.8 , 5.8 ],
-                    [   0.  , 0.  , 0.  , 0.  , 0.  , 1.  , 2.  , 2.  , 3.  , 3.  , 3.  , 4.  ]])
+result3 = np.array([[0., 0., 0., 0., 0., 2., 2.1, 2.9, 2.9, 4.9, 4.9, 4.9],
+                    [np.inf, 1., 1.6, 0.8, 0.5, 4.4, 3., 3.8, 3.8, 5.8, 5.8, 5.8],
+                    [0., 0., 0., 0., 0., 1., 2., 2., 3., 3., 3., 4.]])
 
-@pytest.mark.parametrize("D,result", [(D1,result1),(D2,result2),(D3,result3)])
-def test_filtration(D,result):
+
+@pytest.mark.parametrize("D,result",
+                         [(D1, result1), (D2, result2), (D3, result3)])
+def test_filtration(D, result):
     dgms = newfiltration_persistence(D, max_dim=4)
     births, deaths, dims = [], [], []
     for i, dgm in enumerate(dgms):
